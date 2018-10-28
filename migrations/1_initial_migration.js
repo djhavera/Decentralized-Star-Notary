8cb1a01c30d358a61f4e8a8de5e7173cb9011aa7
David Havera <djhavera@yahoo.com>
2018-10-28 07:15:50 -0400

    Add files via upload

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,1 +1,12 @@
-aa
+//var StarNotary = artifacts.require("./StarNotary.sol");
+
+//module.exports = function(deployer) {
+//  deployer.deploy(StarNotary);
+//};
+
+
+var Migrations = artifacts.require("./Migrations.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
+};
